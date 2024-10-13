a0e9103cd744af5a1d9022af9a2570188801429d
Sergey Green <green.sergey@gmail.com>
2024-10-13 19:08:48 +0200

    -

--- data_ops/csv_op.py
+++ data_ops/csv_op.py
@@ -1,6 +1,6 @@
 import csv
 
-def get_data(file_name: str) -> str:
+def get_data(file_name: str) -> list:
     with open(file_name, newline='') as infile: 
        csv_reader: csv.DictReader = csv.DictReader(infile)
        return [row for row in csv_reader]
